9b882bbcd4e5a99808ebb20d5f6670b2d31fc573
Frida Lindskog <136897910+FridafridaL@users.noreply.github.com>
2024-02-22 16:09:10 +0100

    search

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const SearchForm = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for a movie title here.."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
